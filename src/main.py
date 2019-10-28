63b8401086d76d8f39d1cbbe10a3a65c2b7442de
moirj15 <moirj15@gmail.com>
2019-10-28 13:55:01 -0400

    Working on some minor html compression for data collection for the midpiont evaluation

--- src/main.py
+++ src/main.py
@@ -24,17 +24,25 @@ class HTMLWhiteSpaceRemover(HTMLParser):
     def error(self, message):
         pass
 
+    def _strip_newlines(self, data):
+        return data.replace('\n', '')
+
     def handle_starttag(self, tag, attrs):
         self.output += "<" + tag + ">"
 
     def handle_endtag(self, tag):
-        self.output += "<" + tag + ">"
+        self.output += "</" + tag + ">"
 
     def handle_data(self, data):
-        self.output += data
+        no_newlines = data.replace('\n', '')
+        self.output += no_newlines
+
+    def handle_decl(self, data):
+        self.output += "<!" + self._strip_newlines(data) + ">"
 
 
 class Server(BaseHTTPRequestHandler):
+
 
     def do_HEAD(self):
         pass
@@ -47,24 +55,27 @@ class Server(BaseHTTPRequestHandler):
 
     def handle_http(self, status, content_type):
         filename = "../web-files/python - How to generate random html document - Stack Overflow.html"
-        filename = "../web-files/DOS Interrupts.html"
+        # filename = "../web-files/DOS Interrupts.html"
         # filename = "../web-files/hello.html"
         with open(filename, encoding="utf8") as file:
+            parser = HTMLWhiteSpaceRemover()
             content_type = "text/html"
             response = file.read()
-            response.split()
+            # response.split()
+            parser.feed(response)
             self.send_response(200)
             self.send_header('Content-type', content_type)
             self.end_headers()
-            # response = remove_html_whitespace(response)
-            return bytes(response, "UTF-8")
+            return bytes(parser.output, "UTF-8")
+            # return bytes(response, "UTF-8")
 
     def respond(self):
         content = self.handle_http(200, 'text/html')
         self.wfile.write(content)
 
 
-HOST_NAME = 'localhost'
+HOST_NAME = '10.0.0.18'
+# HOST_NAME = '172.18.55.241'
 PORT_NUMBER = 8000
 
 
